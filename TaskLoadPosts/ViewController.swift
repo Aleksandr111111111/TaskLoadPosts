81e076e6c19869fa413f339e9fba48f509551d3c
Aleksandr111111111 <avkylikov@gmail.com>
2022-01-31 15:16:29 +0200

    Initial Commit

---
+++ TaskLoadPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskLoadPosts
+//
+//  Created by Aleksander Kulikov on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
